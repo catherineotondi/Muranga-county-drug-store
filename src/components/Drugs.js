babba9837b04cbf03ac831a660f8d27155f47186
catherineotondi <catherineotondi@gmail.com>
2023-11-04 11:03:08 +0300

    create a drug component

---
+++ src/components/Drugs.js
@@ -0,0 +1,59 @@
+const Drugs = [
+    {
+        id: "1",
+        Name: "Acetaminophen",
+        Description: "Dosage forms: oral capsule (325 mg; 500 mg), oral granule",
+        Quantity:10
+    },
+    {
+        id: "2",
+        Name: "Bacitracin",
+        Description: "Each gram of ointment contains 500 units of Bacitracin in a low melting special base",
+        Quantity:30
+    },
+    {
+        id: "3",
+        Name: "Yasmin",
+        Description: "is a combination birth control pill containing female hormones that prevent ovulation ",
+        Quantity:50
+    },
+    {
+        id: "4",
+        Name: "Cabenuva",
+        Description: "intramuscular suspension, extended-release (200 mg-300 mg/mL)",
+        Quantity:70
+    },
+    {
+        id: "5",
+        Name: "Paroxetine",
+        Description: "used to treat panic disorder, obsessive-compulsive disorder (OCD), anxiety disorders,",
+        Quantity:50
+    },
+    {
+        id: "6",
+        Name: "Qbrelis",
+        Description: "used in adults to treat congestive heart failure and to improve survival after a heart attack",
+        Quantity:70
+    },
+    {
+        id: "7",
+        Name: "Sacubitril and valsartan",
+        Description: "oral tablet (24 mg-26 mg; 49 mg-51 mg; 97 mg-103 mg)",
+        Quantity:50
+    },
+    {
+        id: "8",
+        Name: "Wixela Inhub",
+        Description: "is a combination product containing a corticosteroid and a long-acting beta2-adrenergic agonist (LABA)",
+        Quantity:90
+    },
+    {
+        id: "9",
+        Name: "Wellbutrin SR",
+        Description: "It is an antidepressant used to treat major depressive disorder and seasonal affective disorder",
+        Quantity:30
+    }
+   
+]
+
+export default Drugs;
